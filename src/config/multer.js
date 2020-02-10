866308d4c4ac7467c27d5327a4985082670bd69f
Victortimoteo <vitortimoteo@hotmail.com>
2020-02-10 20:59:08 -0300

    primeiro commit

---
+++ src/config/multer.js
@@ -0,0 +1,18 @@
+import multer from 'multer';
+import crypto from 'crypto';
+import { extname, resolve } from 'path';
+
+export default {
+  storage: multer.diskStorage({
+    destination: resolve(__dirname, '..', '..', 'tmp', 'uploads'),
+    filename: (req, file, cb) => {
+      crypto.randomBytes(16, (err, res) => {
+        if (err) return cb(err);
+
+        // salvando nome do arquivo de imagem no banco de dados.
+
+        return cb(null, res.toString('hex') + extname(file.originalname));
+      });
+    },
+  }),
+};
