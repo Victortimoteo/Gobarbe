0673e399a4552e20d0aae457732b992cf57fab2d
Victortimoteo <vitortimoteo@hotmail.com>
2020-02-16 22:54:38 -0300

    fila de emails

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'aa91060df716ab',
+    pass: 'e26abf7c0646f4',
+  },
+  default: {
+    from: 'Equipe Gobarber <noreply@gobaber.com>',
+  },
+};
