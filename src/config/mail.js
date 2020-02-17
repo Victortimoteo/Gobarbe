f4e311ff7a47f5786068e188fef1f5a635fe6a17
Victortimoteo <vitortimoteo@hotmail.com>
2020-02-17 20:33:40 -0300

    Variáveis de ambiente

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: 'aa91060df716ab',
-    pass: 'e26abf7c0646f4',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe Gobarber <noreply@gobaber.com>',
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,3 @@
 export default {
-  dsn: 'https://1d4cdfbb8231441cb33d9ddf6b265b7b@sentry.io/2615199',
+  dsn: process.env.SENTRY_DSN,
 };
